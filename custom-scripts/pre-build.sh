ed4b05e54b3d9b42395fc00c4332f58d9b911d29
GabrielWetzel <gabrie.rockenbach@acad.pucrs.br>
2019-08-22 09:26:02 -0300

    Configurando servidor simples

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
+cp $BASE_DIR/../custom-scripts/S50srv $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+chmod +x $BASE_DIR/target/etc/init.d/S50srv
