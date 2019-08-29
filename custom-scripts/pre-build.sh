8585e52e2dacfe5decb2f8c1c52a7bf71c8dc174
GabrielWetzel <gabrie.rockenbach@acad.pucrs.br>
2019-08-29 10:56:32 -0300

    finalizado servidor do trabalho 1

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -2,5 +2,7 @@
 
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
 cp $BASE_DIR/../custom-scripts/S50srv $BASE_DIR/target/etc/init.d
+cp $BASE_DIR/../custom-scripts/simple_http_server.py $BASE_DIR/target/usr/bin
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
 chmod +x $BASE_DIR/target/etc/init.d/S50srv
+chmod +x $BASE_DIR/target/usr/bin/simple_http_server.py
